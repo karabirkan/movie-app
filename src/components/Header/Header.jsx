cb8e0182462266276edca78bac0f7c19c1221178
Birkan Kara <115654236+karabirkan@users.noreply.github.com>
2023-06-27 15:13:07 +0300

    search bar , functionality and scss added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,38 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import user from "../../images/user.png";
 import "./Header.scss";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import {
+  fetchAsyncMovies,
+  fetchAsyncShows,
+} from "../../features/movies/movieSlice";
 const Header = () => {
+  const [term, setTerm] = useState("");
+  const dispatch = useDispatch();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(fetchAsyncMovies(term));
+    dispatch(fetchAsyncShows(term));
+  };
   return (
     <nav className="header">
-      <NavLink to="/">
-        <div className="logo">Movie App</div>
-      </NavLink>
+      <div className="logo">
+        <NavLink to="/">Movie App </NavLink>
+      </div>
+      <div className="search-bar">
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            value={term}
+            placeholder="Search Movies or Shows"
+            onChange={(e) => setTerm(e.target.value)}
+          />
+          <button type="submit">
+            <i className="fa fa-search"></i>
+          </button>
+        </form>
+      </div>
       <div className="user-image">
         <img src={user} alt="" />
       </div>
