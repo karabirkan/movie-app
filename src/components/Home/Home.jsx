d70c4a573a8fdc04c06850265a31ea618b13f076
Birkan Kara <115654236+karabirkan@users.noreply.github.com>
2023-06-27 12:43:00 +0300

    middleware thunk

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,26 +1,14 @@
 import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
 import axios from "axios";
-import movieApi from "../../common/apis/movieApi";
-import { APIKey } from "../../common/apis/MovieApiKey";
 import { useDispatch } from "react-redux";
-import { addMovies } from "../../features/movies/movieSlice";
+import { fetchAsyncMovies } from "../../features/movies/movieSlice";
 
 const Home = () => {
-  const movieText = "Harry";
   const dispatch = useDispatch();
   useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await movieApi
-        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
-        .catch((err) => {
-          console.log(err, "there is an error");
-        });
-      dispatch(addMovies(response.data));
-    };
-
-    fetchMovies();
-  }, []);
+    dispatch(fetchAsyncMovies());
+  }, [dispatch]);
 
   return (
     <div>
